70935d31db5d18b614f96cac4cb372ae2248d1a0
MartaLytvyn <marta.lytvyn.fes@lnu.edu.ua>
2026-04-07 07:38:27 +0300

    Update Dyhotomia_class.cpp

--- Lab.3/Dyhotomia_class.cpp
+++ Lab.3/Dyhotomia_class.cpp
@@ -1,6 +1,8 @@
 #include "Dyhotomia_class.h"
 #include <cmath>
-
+#include <stdexcept>
+#include <iostream>
+using namespace std;
 Dyhotomia_class::Dyhotomia_class() {}
 
 Dyhotomia_class::~Dyhotomia_class() {}
@@ -18,12 +20,27 @@ void Dyhotomia_class::setTolerance(double vol_eps)
 
 double Dyhotomia_class::f(double x)
 {
-    return x * x - 4; }
+    if (fabs(x) < 1e-9) 
+        throw runtime_error("Division by zero in f(x)");
+
+    return cos(2/x) - 2 * sin(1/x) + 1/x;
+}
 
 int Dyhotomia_class::count(double &x)
 {
-    if (f(a) * f(b) > 0)
+    if (fabs(a) < 1e-9 || fabs(b) < 1e-9)
+    {
+        cerr << "Interval contains zero, cannot continue\n";
+        return -2;
+    }
+
+    cout << "f(a) = " << f(a) << ", f(b) = " << f(b) <<endl;
+
+    if (f(a) * f(b) > 0) 
+    {
+        cerr << "Function has same sign at ends of interval\n";
         return -1;
+    }
 
     while (fabs(b - a) > eps)
     {
