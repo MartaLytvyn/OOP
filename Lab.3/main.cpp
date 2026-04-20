fa403eb3c6161cf53be281a83f0dcaa9f8b0e502
MartaLytvyn <marta.lytvyn.fes@lnu.edu.ua>
2026-04-21 01:33:33 +0300

    Update main.cpp

--- Lab.3/main.cpp
+++ Lab.3/main.cpp
@@ -5,32 +5,37 @@ using namespace std;
 int main()
 {
     Newton_class newton;
-    newton.setStart(1.1);       
+    newton.setStart(10);       
     newton.setTolerance(1e-9);  
+    newton.setInterval(1.0, 2.0);
 
     double rootNewton;
     int statusNewton = newton.count(rootNewton);
 
     if (statusNewton == 0)
-        cout << "Root (Newton) = " << rootNewton << std::endl;
+        cout << "Root (Newton) = " << rootNewton << endl;
     else if (statusNewton == -1)
-        cout << "Newton method failed: derivative too small" << std::endl;
+        cout << "Newton method failed: derivative too small" << endl;
+    else if (statusNewton == -2)
+        cout << "Newton method failed: invalid value" << endl;
+    else if (statusNewton == -3)
+        cout << "Newton method failed: too many iterations" << endl;
     else
-        cout << "Newton method failed" << std::endl;
+        cout << "Newton method failed" << endl;
 
 
-    Dyhotomia_class dyho;
-    dyho.setVolumes(1.0, 2.0);  
-    dyho.setTolerance(1e-9);
+    Dyhotomia_class dyh;
+    dyh.setVolumes(1.0, 2.0);  
+    dyh.setTolerance(1e-9);
 
-    double rootDyho;
-    int statusDyho = dyho.count(rootDyho);
+    double rootDyh;
+    int statusDyh = dyh.count(rootDyh);
 
-    if (statusDyho == 0)
-        cout << "Root (Dichotomy) = " << rootDyho << std::endl;
-    else if (statusDyho == -1)
+    if (statusDyh == 0)
+        cout << "Root (Dichotomy) = " << rootDyh << std::endl;
+    else if (statusDyh == -1)
         cout << "Dichotomy method failed: function has same sign at ends" << std::endl;
-    else if (statusDyho == -2)
+    else if (statusDyh == -2)
         cout << "Dichotomy method failed: interval contains zero" << std::endl;
     else
         cout << "Dichotomy method failed" << std::endl;
